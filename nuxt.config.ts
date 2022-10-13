ee8904a0ec4431750e01352601199e237e6efb71
Ngorco <->
2022-10-13 14:39:53 -0400

    feat: initial app code

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  publicRuntimeConfig: {
+    graphqlurl: 'http://localhost:4000/graphql' 
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script setup>
+const query = {query: `
+  {getUser {
+    user_id
+    user_name
+    user_description
+    user_password
+    user_verification_code
+    user_status
+    user_email
+  }}
+`};
+const conf = useRuntimeConfig();
+console.log(conf);
+const {data: {getUser: {user_id}}} = await $fetch(conf.graphqlurl, {
+  body: JSON.stringify(query),
+  method: 'POST',
+  headers: { "Content-Type": "application/json" },
+});
+</script>
+<template>
+  <div>
+    <h1>Hello!</h1> {{user_id}}
+  </div>
+</template>
